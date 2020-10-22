5b3215f96ed5b5cbf05667fe7a38f9690bbcfddb
Alexander Prada <alexpradap@gmail.com>
2020-10-22 17:14:33 -0500

    Ajuste a replicate.sh

--- backstop-solution/replicate.sh
+++ backstop-solution/replicate.sh
@@ -9,7 +9,7 @@ mkdir $NEW_DIR/backstop_data
 cd $NEW_DIR/backstop_data
 ln -s ../../backstop-base/backstop_data/engine_scripts/ engine_scripts
 cd ..
-ln -s ../backstop-base/node_modules/ node_modules
+ln -s ../../node_modules/ node_modules
 cp ../backstop-base/get_report_names.js .
 cp ../backstop-base/get_report_names.html .
 cp ../backstop-base/configure_report.js .
