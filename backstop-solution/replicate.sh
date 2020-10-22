02ecedc67952662036f949e1c009a5d18040877d
Alexander Prada <alexpradap@gmail.com>
2020-10-22 17:05:04 -0500

    Ajuste al script de replicación del directorio base de backstopjs

--- backstop-solution/replicate.sh
+++ backstop-solution/replicate.sh
@@ -6,9 +6,6 @@ NEW_DIR=$filename
 mkdir $NEW_DIR
 mv $NEW_DIR.json $NEW_DIR/$NEW_DIR.json
 mkdir $NEW_DIR/backstop_data
-mkdir $NEW_DIR/backstop_data/bitmaps_reference
-mkdir $NEW_DIR/backstop_data/bitmaps_test
-cp -R backstop-base/backstop_data/html_report $NEW_DIR/backstop_data/html_report/
 cd $NEW_DIR/backstop_data
 ln -s ../../backstop-base/backstop_data/engine_scripts/ engine_scripts
 cd ..
